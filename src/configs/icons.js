c5745728a965f3de7f344df77cff1a386a6a3ed3
cosmoas13 <cosmoas13@gmail.com>
2022-10-09 10:38:55 +0700

    add icon png

--- src/configs/icons.js
+++ src/configs/icons.js
@@ -1,14 +1,14 @@
 const icons = {
-  DASHBOARD: require('../static/icon-dashboard.svg'),
+  DASHBOARD: require('../static/icon-dashboard.png'),
   HAMBURGER: require('../static/icon-hamburger.png'),
-  PROFILE: require('../static/icon-profile.svg'),
+  PROFILE: require('../static/icon-profile.png'),
   LOGOUT: require('../static/icon-logout2x.png'),
   HELP: require('../static/icon-help.png'),
   UP: require('../static/icon-up.png'),
   DOWN: require('../static/icon-down.png'),
-  DOT: require('../static/icon-more.svg'),
-  SHOP: require('../static/icon-sales-over.svg'),
-  ARROW: require('../static/icon-down-arrow.svg'),
+  DOT: require('../static/icon-more.png'),
+  SHOP: require('../static/icon-sales-over.png'),
+  ARROW: require('../static/icon-down-arrow.png'),
   CALENDAR: require('../static/icon-calendar2x.png'),
 };
 
